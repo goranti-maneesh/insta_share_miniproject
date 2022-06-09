e94c9a676190bbf6d74aaeae3dff408deb307505
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2022-06-09 22:40:07 +0530

    mini project

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -6,19 +6,50 @@ RenderInstaLogo, InstaShareTitle, LoginButton, InputFieldContainer,
 LabelElement, InputElement, ErrorMsg} from './StyledComponents'
 // import useOnSuccess from "./OnSuccess";
 
+const userNameErrMsg = "User name must contain 5 letters"
+const passwordErrMsg = "Password must contain letters, special character and number"
+
 export const LoginForm = () => {
     const history = useHistory()
     
     const [userName, setUserName] = useState("")
     const [password, setPassword] = useState("")
+    const [isUserNameErrorDisplayed, setUserNameErrorDisplayStatus] = useState(false)
+    const [isPasswordErrorDisplayed, setPasswordErrorDisplayStatus] = useState(false)
     const [errorMsg, setErrorMsg] = useState("")
     const [isErrorDisplayed, setErrorDisplayStatus] = useState(false)
 
-    const useInputLabelContainer = (type:string, labelText: string, id: string, value:string, onchangeMethod: (event: React.FormEvent<HTMLInputElement>) => void, placeholder: string) => (
+    const OnFocus = (setFunction: { (value: React.SetStateAction<boolean>): void}) => {
+        setFunction(false)
+    }
+
+    const onBlurUsername = () => {
+        const regex = new RegExp('[a-zA-Z0-9]{5,}')
+        const result = regex.test(userName)
+        if(result){
+            setUserNameErrorDisplayStatus(false)
+        }else{
+            setUserNameErrorDisplayStatus(true)
+        }
+    }
+
+    const onBlurPassword = () => {
+        const regex = new RegExp('([!@#$%^&][A-Za-z][0-9]){7,14}')
+        const result = regex.test(password)
+        console.log(result, regex, password)
+        if(result){
+            setPasswordErrorDisplayStatus(false)
+        }else{
+            setPasswordErrorDisplayStatus(true)
+        }
+    }
+
+    const useInputLabelContainer = (type:string, labelText: string, id: string, value:string, onchangeMethod: (event: React.FormEvent<HTMLInputElement>) => void, placeholder: string, isErrorDisplayed: boolean, setFunction: { (value: React.SetStateAction<boolean>): void}, errMsg: string, onblurFunc: React.FocusEventHandler<HTMLInputElement>) => (
         <InputFieldContainer>
             <LabelElement htmlFor={id}>{labelText}</LabelElement>
             <br/>
-            <InputElement type={type} value={value} id={id} onChange={onchangeMethod} placeholder={placeholder}/>
+            <InputElement type={type} value={value} id={id} onChange={onchangeMethod} placeholder={placeholder} onFocus={() => OnFocus(setFunction)} onBlur={onblurFunc}/>
+            {isErrorDisplayed ? <ErrorMsg>{errMsg}</ErrorMsg> : null}
         </InputFieldContainer>
     )
     
@@ -80,10 +111,10 @@ export const LoginForm = () => {
                     <RenderInstaLogo src="https://res.cloudinary.com/degjdup40/image/upload/v1654572262/Standard_Collection_8_m8rwqb.png"/>
                     <InstaShareTitle>Insta Share</InstaShareTitle>
                 </InstaLogoContainer>
-                {useInputLabelContainer("text", "USERNAME", "username", userName, useOnChangeUsername, "Username")}
-                {useInputLabelContainer("password", "PASSWORD", "password", password, useOnChangePassword, "Password")}
-                {isErrorDisplayed ? <ErrorMsg>{errorMsg}</ErrorMsg> : null}
+                {useInputLabelContainer("text", "USERNAME", "username", userName, useOnChangeUsername, "Username", isUserNameErrorDisplayed, setUserNameErrorDisplayStatus, userNameErrMsg, onBlurUsername)}
+                {useInputLabelContainer("text", "PASSWORD", "password", password, useOnChangePassword, "Password", isPasswordErrorDisplayed, setPasswordErrorDisplayStatus, passwordErrMsg, onBlurPassword)}
                 <LoginButton type="submit">Login</LoginButton>
+                {isErrorDisplayed ? <ErrorMsg>{errorMsg}</ErrorMsg> : null}
             </LoginFormContainer>
         </LoginPageContainer>
     )
